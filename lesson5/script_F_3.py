cce3bdeef745234b7e84b427050a6b841f6fb1eb
levonchelo <l.lyamaev@mail.ru>
2024-08-20 03:24:17 +0800

    выполнил дз

---
+++ lesson5/script_F_3.py
@@ -0,0 +1,15 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Firefox()
+
+driver.maximize_window()
+driver.get('http://uitestingplayground.com/classattr')
+
+for i in range(3):
+    driver.find_element(By.CSS_SELECTOR, 'button.btn-primary').click()
+    alert = driver.switch_to.alert
+    alert.accept()
+    driver.refresh()
+
+driver.quit()
